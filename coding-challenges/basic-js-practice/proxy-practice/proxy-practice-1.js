442214b86f599b8c66916b46cafecf37fdffe883
simon61610 <simon410379007@gmail.com>
2026-03-15 16:52:15 +0800

    feat: 新增 JS proxy 練習

---
+++ coding-challenges/basic-js-practice/proxy-practice/proxy-practice-1.js
@@ -0,0 +1,41 @@
+// 請建立一個 Proxy 物件，用來監控使用者資料的修改。
+
+// 請建立一個 proxyUser，讓它在 修改屬性時：
+/* 
+1. 在 console 印出修改訊息: 
+    修改 name 從 Tom 變成 Jack
+2. 再把值真的寫回原本的 user 物件 
+*/
+
+/** 已知資料 */
+const user = {
+  name: "Tom",
+  age: 20
+}
+
+// ==================================================
+// Ans:
+const proxyUser = new Proxy (user, {
+  get(target, prop, receiver){
+    const value = Reflect.get(target, prop, receiver)
+    console.log(`取得 ${prop} 的資料為 ${value}`)
+    return value
+  },
+  
+  set(target, prop, value, receiver){
+    const oldValue = target[prop]
+    console.log(`修改 ${prop} 從 ${oldValue} 變成 ${value}`)
+    return Reflect.set(target, prop, value, receiver)
+  }
+})
+
+// ==================================================
+
+proxyUser.name = 'Simon'
+console.log(proxyUser.name)
+proxyUser.age = 29
+console.log(proxyUser.age)
+
+
+
+
---
+++ coding-challenges/basic-js-practice/proxy-practice/proxy-practice-2.js
@@ -0,0 +1,48 @@
+// 請建立一個 Proxy 物件，用來限制使用者資料的修改。
+
+// 請建立一個 proxyUser，讓它在 修改屬性時：
+
+/*
+1. 如果屬性存在：
+   - 允許修改
+   - console 印出：
+     修改 age 從 20 變成 25
+
+2. 如果屬性不存在：
+   - 禁止新增
+   - console 印出：
+     gender 不存在，禁止新增
+
+3. user 物件不可以被新增新的屬性
+*/
+
+/** 已知資料 */
+const user = {
+  name: "Tom",
+  age: 20
+}
+
+// ==================================================
+// Ans:
+const proxyUser = new Proxy(user, {
+    set(target, prop, value, receiver){
+        const allowModify = () => {
+            console.log(`修改 ${prop} 從 ${target[prop]} 變成 ${value}`)
+            return Reflect.set(target, prop, value, receiver)
+        }
+        const forbidModify = () => {
+            console.log(`${prop} 不存在，禁止新增`)
+            return false
+        }
+
+        return prop in target ? allowModify() : forbidModify()
+    }
+})
+
+
+// ==================================================
+
+proxyUser.age = 25
+proxyUser.gender = "male"
+
+console.log(user)
